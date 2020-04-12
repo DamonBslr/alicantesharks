aea79b77639ac6c4513e30f3840cacc2a7f4109e
DamonBslr <63117642+DamonBslr@users.noreply.github.com>
2020-04-12 18:23:12 +0200

    calendario 1 year

---
+++ js/sharks.js
@@ -0,0 +1,115 @@
+
+//2020
+
+//enero
+
+//febrero
+
+//marzo
+// Set previous button off!!
+function nextabril(){
+	document.getElementById('marzo').className = 'marzo-hide'
+	document.getElementById('abril').className = 'abril-show'
+}
+
+//abril
+function previousmarzo(){
+	document.getElementById('abril').className = 'abril-hide'
+	document.getElementById('marzo').className = 'marzo-show'
+}
+function nextmayo(){
+	document.getElementById('abril').className = 'abril-hide'
+	document.getElementById('mayo').className = 'mayo-show'
+}
+
+//mayo
+function previousabril(){
+	document.getElementById('mayo').className = 'mayo-hide'
+	document.getElementById('abril').className = 'abril-show'
+}
+function nextjunio(){
+	document.getElementById('mayo').className = 'mayo-hide'
+	document.getElementById('junio').className = 'junio-show'
+}
+
+//junio
+function previousmayo(){
+	document.getElementById('junio').className = 'junio-hide'
+	document.getElementById('mayo').className = 'mayo-show'
+}
+function nextjulio(){
+	document.getElementById('junio').className = 'junio-hide'
+	document.getElementById('julio').className = 'julio-show'
+}
+
+//julio
+function previousjunio(){
+	document.getElementById('julio').className = 'julio-hide'
+	document.getElementById('junio').className = 'junio-show'
+}
+function nextagosto(){
+	document.getElementById('julio').className = 'julio-hide'
+	document.getElementById('agosto').className = 'agosto-show'
+}
+
+//agosto
+function previousjulio(){
+	document.getElementById('agosto').className = 'agosto-hide'
+	document.getElementById('julio').className = 'julio-show'
+}
+function nextseptiembre(){
+	document.getElementById('agosto').className = 'agosto-hide'
+	document.getElementById('septiembre').className = 'septiembre-show'
+}
+
+//septiembre
+function previousagosto(){
+	document.getElementById('septiembre').className = 'septiembre-hide'
+	document.getElementById('agosto').className = 'agosto-show'
+}
+function nextoktubre(){
+	document.getElementById('septiembre').className = 'septiembre-hide'
+	document.getElementById('oktubre').className = 'oktubre-show'
+}
+
+//oktubre
+function previousseptiembre(){
+	document.getElementById('oktubre').className = 'oktubre-hide'
+	document.getElementById('septiembre').className = 'septiembre-show'
+}
+function nextnoviembre(){
+	document.getElementById('oktubre').className = 'oktubre-hide'
+	document.getElementById('noviembre').className = 'noviembre-show'
+}
+
+//noviembre
+function previousoktubre(){
+	document.getElementById('noviembre').className = 'noviembre-hide'
+	document.getElementById('oktubre').className = 'oktubre-show'
+}
+function nextdiciembre(){
+	document.getElementById('noviembre').className = 'noviembre-hide'
+	document.getElementById('diciembre').className = 'diciembre-show'
+}
+
+//diciembre
+function previousnoviembre(){
+	document.getElementById('diciembre').className = 'diciembre-hide'
+	document.getElementById('noviembre').className = 'noviembre-show'
+}
+// Set next button off!!
+
+//2021
+
+
+
+
+
+
+
+
+
+
+
+
+
