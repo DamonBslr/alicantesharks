3c6863b0771df1af8af5e2101b49dccc4ad1ba06
DamonBslr <63117642+DamonBslr@users.noreply.github.com>
2020-05-03 17:32:03 +0200

    a whole bunch of stuff

---
+++ header/js/header.js
@@ -0,0 +1,120 @@
+function show() {
+  var x = document.getElementById("primary-menu");
+  if (x.className === "menu") {
+    x.className += " show";
+  } else {
+    x.className = "menu";
+  }
+}
+
+//Footer Color Change
+
+//Insta Logo
+
+
+function instafooter() {
+  var x = document.getElementById("footer1");
+  if (x.className === "footer-logo") {
+    x.className += " insta";
+  } else {
+    x.className = "footer-logo";
+  }
+
+  var x = document.getElementById("footer2");
+  if (x.className === "footer-social") {
+    x.className += " insta";
+  } else {
+    x.className = "footer-social";
+  }
+
+  var x = document.getElementById("footer3");
+  if (x.className === "footer-content") {
+    x.className += " insta";
+  } else {
+    x.className = "footer-content";
+  }
+
+}
+
+function facebookfooter() {
+  var x = document.getElementById("footer1");
+  if (x.className === "footer-logo") {
+    x.className += " facebook";
+  } else {
+    x.className = "footer-logo";
+  }
+
+  var x = document.getElementById("footer2");
+  if (x.className === "footer-social") {
+    x.className += " facebook";
+  } else {
+    x.className = "footer-social";
+  }
+
+  var x = document.getElementById("footer3");
+  if (x.className === "footer-content") {
+    x.className += " facebook";
+  } else {
+    x.className = "footer-content";
+  }
+
+}
+
+function twitterfooter() {
+  var x = document.getElementById("footer1");
+  if (x.className === "footer-logo") {
+    x.className += " twitter";
+  } else {
+    x.className = "footer-logo";
+  }
+
+  var x = document.getElementById("footer2");
+  if (x.className === "footer-social") {
+    x.className += " twitter";
+  } else {
+    x.className = "footer-social";
+  }
+
+  var x = document.getElementById("footer3");
+  if (x.className === "footer-content") {
+    x.className += " twitter";
+  } else {
+    x.className = "footer-content";
+  }
+
+}
+
+
+
+function normalfooter() {
+  var x = document.getElementById("footer1");
+  if (x.className === "footer-logo insta") {
+    x.className = "footer-logo";
+  } else {
+    x.className = "footer-logo";
+  }
+
+  var x = document.getElementById("footer2");
+  if (x.className === "footer-social insta") {
+    x.className = "footer-social";
+  } else {
+    x.className = "footer-social";
+  }
+   
+  var x = document.getElementById("footer3");
+  if (x.className === "footer-content insta") {
+    x.className = "footer-content";
+  } else {
+    x.className = "footer-content";
+  }
+
+
+}
+
+// Footer 2 Social Links Grid
+
+
+
+
+
+
--- php/index.php
+++ php/index.php
@@ -22,37 +22,7 @@
 <body>
 
 <!--Desktop Header-->
-	<header class="desktop">
-		<!-- Grid Style -->
-		<ul class="grid">
-			<!-- Logo -->
-			<a href="https://damonbslr.github.io/alicantesharks/"><img class="logo" src="img/sharks-logo.png" width="150" height="150"></a>
-
-			<!-- Heading -->
-			<div class="heading"><h1>Alicante Sharks Club Football Americano</h1>
-
-				<!-- Navigation -->
-				<div class="nav-grid">
-					<div class="nav-element"><a href="noticias">Noticias</a></div>
-					<div class="nav-element hover"><a href="equipo">Equipo</a>
-						<div class="navisub">
-							<div class="nav-sub-element first"><a href="equipo/senior-masculino">Senior Masculino</a></div>
-							<div class="nav-sub-element"><a href="equipo/senior-femenino">Senior Femenino</a></div>
-							<div class="nav-sub-element"><a href="equipo/junior-masculino">Junior Masculino</a></div>
-							<div class="nav-sub-element"><a href="equipo/cadetes-mixto">Cadetes Mixto</a></div>
-							<div class="nav-sub-element"><a href="equipo/flag-masculino">Flag Masculino</a></div>
-							<div class="nav-sub-element"><a href="equipo/flag-femenino">Flag Femenino</a></div>
-	
-						</div>
-					</div>
-
-					<div class="nav-element"><a href="calendario">Calendario</a></div>
-
-					<div class="nav-element"><a href="clasificacion">Clasificación</a></div>
-				</div>
-			</div>
-		</ul>
-	</header>
+<?php include 'header.php';?>
 
 <!--Mobile Header-->
 	<header class="mobile">
