43ca0ec60678d400f21fb3cc5c317a48da229004
DamonBslr <63117642+DamonBslr@users.noreply.github.com>
2020-05-24 14:31:13 +0200

    mh

---
+++ index.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Font -->
+	<script src="https://kit.fontawesome.com/480f986a57.js" crossorigin="anonymous"></script>
+	<link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Francois+One&family=Oswald&display=swap" rel="stylesheet">
+	<!-- Stylesheets -->
+	<link rel="stylesheet" type="text/css" href="css/grid.css">
+	<link rel="stylesheet" type="text/css" href="css/menu.css">
+	<link rel="stylesheet" type="text/css" href="css/sharks.css">
+	<link rel="stylesheet" type="text/css" href="css/header.css">
+	<link rel="stylesheet" type="text/css" href="css/footer.css">
+	<!-- Java Script -->
+	<script type="text/javascript" src="js/sharks.js"></script>
+	<!-- Icon -->
+	<link rel="icon" href="img/logos-sharks/logosharks-s.png">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>Alicante Sharks Club Football Americano</title>
+</head>
+
+<body>
+
+	<!-- Header
+	---------------------------------------------------------------->
+	<?php include 'header.php';?>
+
+	<!--Dia
+	------------------------------------------------------->
+		<div class="row">
+			<div class="col-s-12 col-m-12">
+				<div class="dia mobile">
+					<img class="logo" src="img/logos-sharks/logotipo-shark.png">
+				</div>
+				<div class="dia desktop">
+					<img src="img/sharksbanner1.jpg">
+				</div>
+			</div>
+		</div>
+
+	<!-- Important News
+	---------------------------------------------------------->		
+	<div class="row">
+		<div class="col-12">
+			<div class="news">
+				<div class="news-content">
+					<h2>COMUNICADO OFICIAL</h2>
+					<h4>Desde el pasado miércoles se han cancelado todas las actividades del club.<br> Ahora más que nunca toda precaución es poca, nosotros también nos quedamos en casa. <br>¡¡Esperamos volver lo antes posible y de la mejor manera!!</h4>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!--Main
+	-------------------------------------------------------------------------------------------------->
+	<main>
+
+		<!--News line
+		--------------------------------------------------------->
+			<div class="row">
+				<div class="col-12">
+					<div class="line">
+						<h3>News</h3>
+					</div>
+				</div>
+			</div>
+
+
+
+		<!--Blog Row 1
+		-------------------------------------------------------------------->
+			<div class="row">
+				<div class="col-s-12 col-12">
+					<div class="blog-grid">
+
+							<!-- Left Blog Container 1
+							--------------------------------------->
+							<div class="blog-container left">
+								<div class="blog-image half">
+									<a href="/noticias" class="blog-image-link">
+										<img src="img/blog/juniorvsfirebatslong.jpg">
+									</a>
+								</div>
+								<div class="blog-text half">
+									<h3>
+										ALC Sharks - VLC Firebats LVFA 26/01/2019
+									</h3><br>
+									<p>
+										Partido de ida contra Valencia Firebats disputado en Alicante. ¡Qué ganas tenemos de volver a los game day!⁣
+									</p>
+									<div class="blog-link desktop">
+										<a href="#">Leer mas</a>
+									</div>
+								</div>
+							</div>
+							
+
+							<!-- Right Blog Container 1
+						--------------------------------------->
+						<div class="blog-container left">
+							<div class="blog-image half">
+								<a href="/noticias" class="blog-image-link">
+									<img src="img/blog/sharkschoollong.jpg">
+								</a>
+							</div>
+							<div class="blog-text half">
+								<h3>
+									¡Alicante Sharks en los colegios!
+								</h3><br>
+								<p>
+									Antes de la cuarentena, pudimos participar en varios colegios enseñándoles qué es el fútbol americano, sus fundamentos básicos y algunos ejercicios.
+								</p>
+								<a href="#" class="blog-link desktop">Leer mas</a>
+							</div>
+						</div>
+					</div>
+				</div>	
+			</div>
+
+
+	<!--Blog Row 2
+	-------------------------------------------------------------------->
+		<div class="row">
+			<div class="col-s-12 col-12">
+				<div class="blog-grid">
+
+						<!-- Left Blog Container 1
+							--------------------------------------->
+							<div class="blog-container left quarter">
+								<div class="blog-image quarter">
+									<a href="/noticias" class="blog-image-link">
+										<img src="img/blog/4blooms.jpg">
+									</a>
+								</div>
+								<div class="blog-text quarter">
+									<h3>
+										¡4blooms sirvió de nuevo!
+									</h3><br>
+									<p>
+										Nos complace comunicar que nuestro patrocinador <a href="https://www.instagram.com/4bloomsalicante/" target="_blank">4blooms</a> vuelve a ofrecer servicio.⁣⁣⁣⁣⁣⁣⁣⁣
+										<a href="#" class="blog-link desktop">Leer mas</a>
+									</p>
+								</div>
+							</div>
+							<!--------------------------------------->
+
+						
+						<!-- Right Blog Container 2
+						--------------------------------------->
+						<div class="blog-container right quarter">
+							<div class="blog-image quarter">
+								<a href="/noticias" class="blog-image-link">
+									<img src="img/blog/sharks-sponsor.jpg">
+								</a>
+							</div>
+							<div class="blog-text quarter">
+								<h3>
+									¡Muchas gracias!
+								</h3><br>
+								<p>
+									Hoy queremos agradecer a los patrocinadores..
+								</p>
+								<div class="blog-link desktop" >
+									<a href="#">Leer mas</a>
+								<div>
+							</div>
+						</div>
+						<!--------------------------------------->
+
+				</div>
+			</div>	
+		</div>
+
+
+	</main>
+
+	<!-- Footer
+	----------------------------------------------------------------------------------------->
+	<footer>
+		<div class="row footer" id="footer">
+			<div class="col-12">
+				<div class="footer-logo">
+					<img src="img/logos-sharks/logotipo-shark.png">
+				</div>
+				<div class="social-links-grid">
+					<div class="follow-instagram">
+						<a target="_blank" href="https://instagram.com/alicantesharks">
+							<i class="fab fa-instagram-square" onmouseover="instafooter()" onmouseout="normalfooter()"></i>
+						</a>
+					</div>
+					<div class="follow-facebook">
+						<a target="_blank" href="https://www.facebook.com/AlicanteSharks/">
+							<i class="fab fa-facebook-square" onmouseover="facebookfooter()" onmouseout="normalfooter()"></i>
+						</a>
+					</div>
+					<div class="follow-twiter">
+						<a target="_blank" href="https://twitter.com/alicantesharks">
+							<i class="fab fa-twitter-square" onmouseover="twitterfooter()" onmouseout="normalfooter()"></i>
+						</a>
+					</div>
+				</div>
+
+				<div class="footer-content">
+					<div class="footer-grid">
+						<div class="footer-grid-left">
+							<div class="footer-heading">	
+								<h3>Patrocinadores</h3>
+							</div>
+							<div class="footer-links">
+								<a href="https://damonbslr.github.io/alicantesharks/patrocinadores">Nuestros patrocinadores</a>
+								<a href="https://damonbslr.github.io/alicantesharks/convertirse-patrocinador">convertirse en patrocinador</a>
+							</div>
+						</div>
+						
+						<div class="footer-grid-middle">
+							<div class="footer-heading">
+								<h3>Enlaces</h3>
+							</div>
+							<div class="footer-links">
+								<a href="https://www.fefa.es/" target="_blank">fefa</a>
+							</div>
+						</div>
+						
+						<div class="footer-grid-right">
+							<div class="footer-heading">
+								<h3>Contacto</h3>
+							</div>
+							<div class="footer-links">
+								<a href="https://damonbslr.github.io/alicantesharks/contacto">Contacto</a>
+								<a href="https://damonbslr.github.io/alicantesharks/aviso-legal">aviso legal</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="damon desktop">
+				<a href="https://www.instagram.com/damon.bslr" target="_blank">created by Damon Basler</a>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
